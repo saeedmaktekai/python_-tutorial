b5c3d37ef04f3a501f7756a08bb300e6d88de538
SAEED <get.maktek@gmail.com>
2024-07-09 00:52:37 +0500

    basic python

---
+++ forandwhileloop.py
@@ -0,0 +1,19 @@
+# mylist=[1,2,3,4,5,6]
+
+# for n in mylist:
+#    print(n)
+
+
+
+i=0 
+while i<5:
+   i+=1
+   if i== 3 :
+      continue
+   print(i)
+   if i == 4 :
+      break
+else :
+   print ("i is not larger then 5 ")
+   
+
